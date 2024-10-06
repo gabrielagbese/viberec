18dac24a19b68b5a38a79d12fb13b6c1d944885b
Gabriel Agbese <gabriel.agbese2@gmail.com>
2024-10-06 23:51:53 +0100

    no reasons test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -627,9 +627,9 @@ Give only the JSON response with no additional text.`;
                                                     </h3>
                                                 </Accordion.Trigger>
                                                 <Accordion.Content className="accordion-content">
-                                                    {selectedMovie?.reason
-                                                        ?.split("-")
-                                                        ?.map((item, index) => {
+                                                    {/* {selectedMovie.reason
+                                                        .split("•")
+                                                        .map((item, index) => {
                                                             const trimmedItem =
                                                                 item.trim();
                                                             return (
@@ -652,7 +652,7 @@ Give only the JSON response with no additional text.`;
                                                                         : trimmedItem}
                                                                 </div>
                                                             );
-                                                        })}
+                                                        })} */}
                                                 </Accordion.Content>
                                             </Accordion.Item>
                                             <Accordion.Item
